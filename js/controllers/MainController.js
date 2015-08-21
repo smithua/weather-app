cbea97e7c6d528cf7b1851751f4c41920ff00c83
smithua <smithuaa@gmail.com>
2015-08-21 16:20:37 +0300

    remove console.log's

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -4,33 +4,27 @@ app.controller('MainController', ['$scope', 'forecast', function($scope, forecas
             forecast.getWeatherData(city).then(function (data) {
                 $scope.tomorrow = data;
                 $scope.tomorrowList = data.list;
-                console.log($scope.tomorrowList);
             });
 
             forecast.getWeatherData2(city).then(function (data) {
                 $scope.tomorrowList2 = data;
-                console.log($scope.tomorrowList2);
             });
 
             forecast.getWeatherData3(city).then(function (data) {
                 $scope.tomorrowList3 = data;
-                console.log($scope.tomorrowList3);
             });
         } else {
             forecast.getWeatherData(newCity).then(function (data) {
                 $scope.tomorrow = data;
                 $scope.tomorrowList = data.list;
-                console.log($scope.tomorrowList);
             });
 
             forecast.getWeatherData2(newCity).then(function (data) {
                 $scope.tomorrowList2 = data;
-                console.log($scope.tomorrowList2);
             });
 
             forecast.getWeatherData3(newCity).then(function (data) {
                 $scope.tomorrowList3 = data;
-                console.log($scope.tomorrowList3);
             });
         };
     });
